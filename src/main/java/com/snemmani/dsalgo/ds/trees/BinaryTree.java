989c84324d1e7f4ef0e25e68d85086a9281fe88a
Shankar Nag Nemmani <shankar.nemmani@yahoo.com>
2020-09-30 15:33:59 +0530

    Implement Binary Tree

--- src/main/java/com/snemmani/dsalgo/ds/trees/BinaryTree.java
+++ src/main/java/com/snemmani/dsalgo/ds/trees/BinaryTree.java
@@ -1,6 +1,6 @@
 package com.snemmani.dsalgo.ds.trees;
 
-public class BinaryTree implements BinarySearchTree {
+public class BinaryTree {
     private Node root;
 
     public BinaryTree(int key) {
@@ -14,41 +14,91 @@ public class BinaryTree implements BinarySearchTree {
     public Node getRoot() {
         return root;
     }
-
-    @Override
+    
     public void insert(int key) {
-        insertIntoNode(key, root);
+        insert(key, root);
     }
 
-    private void insertIntoNode(int key, Node node) {
-        if ( node == null ) {
-            node = new Node(key);
+    private Node insert(int key, Node node) {
+        if ( node == null) {
+            return new Node(key);
         }
 
-        if ( node.getKey() < key ) {
-            insertIntoNode(key, node.getLeft());
+        if ( node.getKey() > key ) {
+            node.setLeft(insert(key, node.getLeft()));
         } else {
-            insertIntoNode(key, node.getRight());
+            node.setRight(insert(key, node.getRight()));
+        }
+
+        return node;
+    }
+
+    
+    public Node search(int key) {
+        return search(key, root);
+    }
+
+    private Node search(int key, Node node) {
+        if ( node == null ) {
+            return null;
+        }
+
+        if ( node.getKey() == key ) {
+            return node;
+        } else if ( node.getKey() < key ) {
+            return search(key, node.getRight());
+        } else {
+            return search(key, node.getLeft());
         }
     }
 
-    @Override
-    public Node search(int key) {
-        return null;
+    
+    public String preOrderTraverse() {
+        return preOrderTraverse(root);
     }
 
-    @Override
-    public void preOrderTraverse() {
+    private String preOrderTraverse(Node node) {
+        if ( node == null) return "";
 
+        String traverse = "";
+        traverse += node.getKey() + " ";
+        if ( node.getLeft() != null )
+            traverse += preOrderTraverse(node.getLeft()) + " ";
+        if ( node.getRight() != null )
+            traverse += preOrderTraverse(node.getRight()) + " ";
+        return traverse.trim();
     }
 
-    @Override
-    public void inOrderTraverse() {
-
+    
+    public String inOrderTraverse() {
+        return inOrderTraverse(root);
     }
 
-    @Override
-    public void postOrderTraverse() {
+    private String inOrderTraverse(Node node) {
+        if ( node == null ) {
+            return "";
+        }
+        String traverse = "";
+        traverse += inOrderTraverse(node.getLeft()) + " ";
+        traverse += node.getKey() + " ";
+        traverse += inOrderTraverse(node.getRight()) + " ";
+        return traverse.trim();
+    }
 
+    
+    public String postOrderTraverse() {
+        return postOrderTraverse(root);
+    }
+
+    private String postOrderTraverse(Node node) {
+        if ( node == null) return "";
+
+        String traverse = "";
+        if ( node.getLeft() != null )
+            traverse += postOrderTraverse(node.getLeft()) + " ";
+        if ( node.getRight() != null )
+            traverse += postOrderTraverse(node.getRight()) + " ";
+        traverse += node.getKey() + " ";
+        return traverse.trim();
     }
 }
--- src/test/java/com/snemmani/dsalgo/ds/trees/BinaryTreeTest.java
+++ src/test/java/com/snemmani/dsalgo/ds/trees/BinaryTreeTest.java
@@ -1,20 +1,57 @@
 package com.snemmani.dsalgo.ds.trees;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BinaryTreeTest {
+    private BinaryTree testTree = null;
+
+    public BinaryTree createTree() {
+        BinaryTree binaryTree = new BinaryTree(2);
+        binaryTree.insert(3);
+        binaryTree.insert(4);
+        binaryTree.insert(1);
+        return binaryTree;
+    }
+
+    @Before
+    public void setUp() {
+        this.testTree = createTree();
+    }
+
     @Test
     public void testTreeCreation() {
-        BinaryTree binaryTree = new BinaryTree(2);
-        assert binaryTree.getRoot().getKey() == 2;
+        assert this.testTree.getRoot().getLeft().getKey() == 1;
+        assert this.testTree.getRoot().getRight().getRight().getKey() == 4;
+    }
 
-        binaryTree.getRoot().setLeft(new Node(3));
-        binaryTree.getRoot().setRight(new Node(4));
+    @Test
+    public void testTreeSearch() {
+        assert this.testTree.search(3).equals(this.testTree.getRoot().getRight());
+        assert this.testTree.search(4).equals(this.testTree.getRoot().getRight().getRight());
+        assert this.testTree.search(8) == null;
+    }
 
-        assert binaryTree.getRoot().getLeft().getKey() == 3;
-        assert binaryTree.getRoot().getRight().getKey() == 4;
+    @Test
+    public void testInOrderTraverse() {
+        assert this.testTree.inOrderTraverse().equals("1 2 3 4");
+    }
+
+    @Test
+    public void testPreOrderTraverse() {
+        assert this.testTree.preOrderTraverse().equals("2 1 3 4");
+    }
+
+    @Test
+    public void testPostOrderTraverse() {
+        assert this.testTree.postOrderTraverse().equals("1 4 3 2");
+    }
+
+    @Test
+    public void testEmptyConstructor() {
+        assert new BinaryTree().getRoot() == null;
     }
 }
